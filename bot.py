5c4073612447ceeae62a67211f2c2f64a66532c4
WorldGreatProjects <55355910+WorldGreatProjects@users.noreply.github.com>
2019-09-18 13:38:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,31 +9,29 @@ from adidas import GetAdidas
 from end import GetEnd
 from asos import GetAsos
 from puma import Puma
-from yoox import Yoox
 from lamodaparse import Lamoda
 from reebok import Reebok
 
 
 TOKEN : str= "821455349:AAEPBbnU4hCsjcKpP3BbM2sxwsayTx_DMGg"
-chat_id : str = ""
 
 
-def GetClothing(message: Message):
+def GetClothing():#message: Message):
         headers : dict = { "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
         item_list = []
         item_list.extend(GetEnd(Flag = False))
         item_list.extend(GetAsos(Flag = False))
-        #item_list.extend(Lamoda())
+        item_list.extend(Lamoda())
         if len(item_list) == 0:
                 with open("log.txt" , "a") as f:
                         f.write(str(time.ctime()) + " GLOBAL ERROR!!!\n")
                 return None
         item = item_list.pop(random.randint(0,len(item_list)-1))
         image = requests.get(item.img_link, headers=headers)
-        bot.send_photo(message.chat.id,image.content ,caption = item.info())
+        bot.send_photo("@bigweargang",image.content ,caption = item.info())
         item_list.clear()
 
-def GetSneakers(message: Message):
+def GetSneakers():#message: Message):
         headers : dict = { "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"}
         item_list = []
         item_list.extend(GetEnd(Flag = True))
@@ -47,7 +45,7 @@ def GetSneakers(message: Message):
                 return None
         item = item_list.pop(random.randint(0,len(item_list)-1))
         image = requests.get(item.img_link, headers=headers)
-        bot.send_photo(message.chat.id,image.content ,caption = item.info())
+        bot.send_photo("@bigweargang",image.content ,caption = item.info())
         item_list.clear()
         
 
@@ -55,22 +53,15 @@ def GetSneakers(message: Message):
 
         
 bot = telebot.TeleBot(TOKEN)
-@bot.message_handler(commands = ['start'])
-def main(message: Message):
-        if message.chat.id == "":
-                pass
-        else:
-                pass
-        if message.text == "/start":
-                #schedule.every(3).seconds.do(GetSneakers,message)
-                #schedule.every(8).seconds.do(GetClothing,message)
-                schedule.every().day.at("13:00").do(GetSneakers,message)
-                schedule.every().day.at("03:00").do(GetClothing,message)
-                while True:
-                        schedule.run_pending()
-                        time.sleep(1)
 
+def main():
+        schedule.every().day.at("13:00").do(GetSneakers)
+        schedule.every().day.at("03:00").do(GetClothing)
+        while True:
+                schedule.run_pending()
+                time.sleep(1)
 
-
-
-bot.polling(True)
+        
+while True:
+        main()
+        bot.polling(True)
